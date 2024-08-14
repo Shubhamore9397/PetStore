d605cdc9462fef359034d9881b7370923e8663ff
Shubhamore9397 <shubhamore9397@gmail.com>
2024-08-14 11:05:37 +0530

    Created pay.html file, updated views.py, urls.py files and also added products to the homepage

--- petstore/petapp/urls.py
+++ petstore/petapp/urls.py
@@ -30,7 +30,13 @@ urlpatterns = [
     path('addtocart/<petid>', views.addtocart),
     path('showcart', views.showUserCart),
     path('removepet/<cartid>', views.removeCart),
-    path('updatecart/<opr>/<cartid>',views.updateCart) 
+    path('updatecart/<opr>/<cartid>',views.updateCart) ,
+    path('search/<pet_type>', views.searchByType),
+    path('range',views.searchByRange),
+    path('sort/<dir>',views.sortByPrice),
+    path('confirmorder',views.confirmOrder),
+    path('makepayment',views.makepayment),
+    path('placeorder',views.placeOrder),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
